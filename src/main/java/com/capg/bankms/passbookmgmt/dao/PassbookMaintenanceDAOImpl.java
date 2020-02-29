e3553f0cd3f824c233f7ac48c355ede6aaa9311b
Admin <vaibhavmalviya1162@gmail.com>
2020-02-29 18:14:41 +0530

    passbookmgt, updatePassbook

--- src/main/java/com/capg/bankms/passbookmgmt/dao/PassbookMaintenanceDAOImpl.java
+++ src/main/java/com/capg/bankms/passbookmgmt/dao/PassbookMaintenanceDAOImpl.java
@@ -1,18 +1,35 @@
 package com.capg.bankms.passbookmgmt.dao;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import com.capg.bankms.accountmgmt.entities.Account;
+import com.capg.bankms.transmgmt.dao.TransactionDaoImpl;
+import com.capg.bankms.transmgmt.entities.Transaction;
 
 public class PassbookMaintenanceDAOImpl implements PassbookMaintenanceDAO {
-
+   
+	
 		
 	@Override
 	public List<Transaction> updatePassboook(String account) {
-		
+	 Map<String,Transaction>transactionStore=TransactionDaoImpl.transactionStore;
+	
+	  for (Map.Entry<String,Transaction> entry : transactionStore.values()) 
+	  {
+		  if(account=transactionStore.values())
+			  
+	  
+		  
+	  }
+	 transactionStore.values();	
 	}
 
 	@Override
-	public List<Transaction> accountSummary(String account, LocalDate fromDate,
+	public List<Transaction> accountSummary(String accountId, LocalDate fromDate,
 			LocalDate toDate) {
 		
 	}
